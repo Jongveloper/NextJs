2f7b8b29913ce32bd831362ce52e36e1d2219c6f
Woo Dy <kjop7419@naver.com>
2022-03-27 16:51:56 +0900

    :art: 프로필 페이지

--- prepare/front/pages/profile.js
+++ prepare/front/pages/profile.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import AppLayout from '../components/AppLayout';
 import Head from 'next/head';
+import FollowList from '../components/FollowList';
+import NicknameEditForm from '../components/NicknameEditForm';
+
 const Profile = () => {
+  const followerList = [{ nickname: '초롱초롱무지' }, { nickname: '상어악어가죽' }, { nickname: '펩시제로슈가' }];
+  const followingList = [{ nickname: '초롱초롱무지' }, { nickname: '상어악어가죽' }, { nickname: '펩시제로슈가' }];
   return (
     <>
       <Head>
         <title>내 프로필 | NodeBird</title>
       </Head>
-      <AppLayout>내 프로필</AppLayout>
+      <AppLayout>
+        <NicknameEditForm />
+        <FollowList header="팔로잉 목록" data={followingList} />
+        <FollowList header="팔로워 목록" data={followerList} />
+      </AppLayout>
     </>
   );
 };
