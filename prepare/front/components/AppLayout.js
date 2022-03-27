33e5fafcbf67114468b5b333398e1d82471f4b83
Woo Dy <kjop7419@naver.com>
2022-03-27 12:14:07 +0900

    :zap: 그리드 및 로그인 폼

--- prepare/front/components/AppLayout.js
+++ prepare/front/components/AppLayout.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import Link from 'next/link';
-import { Menu } from 'antd';
+import { Input, Menu, Row, Col } from 'antd';
 
+import UserProfile from '../components/UserProfile';
+import LoginForm from '../components/LoginForm';
 const AppLayout = ({ children }) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   return (
     <div>
       <Menu mode="horizontal">
@@ -18,12 +21,31 @@ const AppLayout = ({ children }) => {
           </Link>
         </Menu.Item>
         <Menu.Item>
+          <Input.Search enterButton style={{ verticalAlign: 'middle' }} />
+        </Menu.Item>
+        <Menu.Item>
           <Link href="/signup">
             <a>회원가입</a>
           </Link>
         </Menu.Item>
       </Menu>
-      {children}
+      <Row gutter={8}>
+        <Col xs={24} md={6}>
+          {isLoggedIn ? <UserProfile/> : <LoginForm/>}
+        </Col>
+        <Col xs={24} md={6}>
+          {children}
+        </Col>
+        <Col xs={24} md={6}>
+          <a
+            href="https://destory.tistory.com/"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            woody
+          </a>
+        </Col>
+      </Row>
     </div>
   );
 };
