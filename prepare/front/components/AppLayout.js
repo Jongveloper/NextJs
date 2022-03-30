14f229307e7b93b699831613d009d4ec42cddec0
Woo Dy <kjop7419@naver.com>
2022-03-30 23:43:37 +0900

    :zap: slick

--- prepare/front/components/AppLayout.js
+++ prepare/front/components/AppLayout.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import Link from 'next/link';
-import styled from 'styled-components';
+import styled, { createGlobalStyle } from 'styled-components';
 import { useSelector } from 'react-redux';
 import { Input, Menu, Row, Col } from 'antd';
 
@@ -11,12 +11,29 @@ import LoginForm from '../components/LoginForm';
 const SearchInput = styled(Input.Search)`
   vertical-align: middle;
 `;
+
+const Global = createGlobalStyle`
+  .ant-row{
+    margin-right: 0 !important;
+    margin-left: 0 !important;
+  }
+
+  .ant-col:first-child{
+    padding-left: 0 !important;
+  }
+
+  .ant-col:last-child{
+    padding-right: 0 !important;
+  }
+
+`;
 const AppLayout = ({ children }) => {
   const isLoggedIn = useSelector((state) => state.user.isLoggedIn);
   // const {isLoggedIn} = useSelector((state) => state.user);
 
   return (
     <div>
+      <Global />
       <Menu mode="horizontal">
         <Menu.Item>
           <Link href="/">
