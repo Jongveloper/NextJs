982a6c6ef4e19bfdbaee9022b58abd5e2bf84466
Woo Dy <kjop7419@naver.com>
2022-04-14 22:23:48 +0900

    :zap: 서버사이드 렌더링

--- prepare/front/reducers/index.js
+++ prepare/front/reducers/index.js
@@ -4,18 +4,18 @@ import user from './user';
 import post from './post';
 
 // (이전상태, 액션) => 다음 상태
-const rootReducer = combineReducers({
-  index: (state = {}, action) => {
-    switch (action.type) {
-      case HYDRATE:
-        return { ...state, ...action.payload };
-
-      default:
-        return state;
+const rootReducer = (state, action) => {
+  switch (action.type) {
+    case HYDRATE:
+      return action.payload;
+    default: {
+      const combinedReducer = combineReducers({
+        user,
+        post,
+      });
+      return combinedReducer(state, action);
     }
-  },
-  user,
-  post,
-});
+  }
+};
 
 export default rootReducer;
